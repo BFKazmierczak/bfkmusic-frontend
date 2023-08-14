10a76ec379306820d53841172dde74da957fdcf9
BFKazmierczak <bfkazmierczak@gmail.com>
2023-08-15 01:31:57 +0200

    feat: add conditional rendering based on session on the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,29 @@
+import { authOptions } from '@/lib/auth'
+import { getServerSession } from 'next-auth'
 import Link from 'next/link'
+import LoginButton from '../components/Buttons/LoginButton'
 
-const HomePage = () => {
+const HomePage = async () => {
+  const session = await getServerSession(authOptions)
+
   return (
     <div className=" selection:bg-pink-600 mt-[100px] flex flex-col items-center ">
-      <p className=" font-bold text-[48px]">Witaj na platformie BFK Music</p>
+      <p className=" font-bold text-[48px]">
+        {session ? 'Witaj ponownie!' : 'Witaj na platformie BFK Music'}
+      </p>
       <p className=" text-[24px]">
         Jesteś w miejscu, w którym wszystko masz pod ręką.
       </p>
 
-      <Link className=" mt-5 basic-button" href="/register">
-        Zarejestruj się
-      </Link>
+      {!session && (
+        <div className=" flex flex-col items-center gap-y-2 mt-5">
+          <Link className=" basic-button" href="/register">
+            Zarejestruj się
+          </Link>
+          LUB
+          <LoginButton />
+        </div>
+      )}
     </div>
   )
 }
