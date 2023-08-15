8a24d99193cded2b091f05be1abbe15a974f6bc1
BFKazmierczak <bfkazmierczak@gmail.com>
2023-08-15 14:32:34 +0200

    feat(components): add UserAvatar.tsx

---
+++ src/components/Buttons/UserAvatar.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import AccountCircleIcon from '@mui/icons-material/AccountCircle'
+import { useState } from 'react'
+import PopupMenu from '../Menus/PopupMenu/PopupMenu'
+
+const UserAvatar = () => {
+  const [menuOpen, setMenuOpen] = useState<boolean>(false)
+
+  return (
+    <div className=" relative ">
+      <AccountCircleIcon
+        className=" cursor-pointer active:text-neutral-500 transition-all ease-in-out"
+        style={{ fontSize: '3em' }}
+        onClick={() => setMenuOpen((prev) => !prev)}
+      />
+
+      <PopupMenu open={menuOpen} setOpen={setMenuOpen} />
+
+      {/* <div
+        className={` absolute ${
+          !menuOpen && ' hidden'
+        } right-1 bg-neutral-400`}>
+        DUPSKOoo{' '}
+      </div> */}
+    </div>
+  )
+}
+
+export default UserAvatar
