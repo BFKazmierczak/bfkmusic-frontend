34642b24a78107abaa39b7d479f633da1d7d2cf7
BFKazmierczak <bfkazmierczak@gmail.com>
2023-09-04 21:52:09 +0200

    feat: wrap GET_SONGS in graphql

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,39 +1,43 @@
 import { getClient } from '@/lib/client'
 import AudioPlayer from '@/src/components/Media/AudioPlayer'
 import SongList from '@/src/components/Media/SongList'
+import { graphql } from '@/src/gql'
+import { SongEntity, SongEntityResponseCollection } from '@/src/gql/graphql'
 import { SongsResult } from '@/src/interfaces/SongsResult'
 import { ApolloQueryResult, gql } from '@apollo/client'
 
-const GET_SONGS = gql`
-  query GetSongs($pagination: PaginationArg) {
-    songs(pagination: $pagination) {
-      data {
-        id
-        attributes {
-          name
-          description
-          audio {
-            data {
-              id
-              attributes {
-                name
-                alternativeText
-                caption
-                url
+const GET_SONGS = graphql(
+  `
+    query GetSong($pagination: PaginationArg) {
+      songs(pagination: $pagination) {
+        data {
+          id
+          attributes {
+            name
+            description
+            audio {
+              data {
+                id
+                attributes {
+                  name
+                  alternativeText
+                  caption
+                  url
+                }
               }
             }
+            createdAt
+            updatedAt
+            publishedAt
           }
-          createdAt
-          updatedAt
-          publishedAt
         }
       }
     }
-  }
-`
+  `
+)
 
 const CatalogPage = async () => {
-  const result: ApolloQueryResult<SongsResult> = await getClient().query({
+  const result = await getClient().query({
     query: GET_SONGS,
     variables: {
       pagination: {
@@ -42,7 +46,8 @@ const CatalogPage = async () => {
       }
     }
   })
-  const songs = result.data.songs.data
+
+  const songs = result.data.songs?.data as SongEntity[]
 
   return (
     <>
