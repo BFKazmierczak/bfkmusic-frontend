10b4fd9e4e83d2db00e88a397d01e73b7e40941d
BFKazmierczak <bfkazmierczak@gmail.com>
2023-09-12 22:25:16 +0200

    refactor(SongList): remove name and url prop

--- src/components/Media/SongList.tsx
+++ src/components/Media/SongList.tsx
@@ -10,12 +10,7 @@ const SongList = ({ initialSongs }: SongListProps) => {
   return (
     <div className=" flex flex-col gap-y-5 mt-10">
       {initialSongs?.map((song) => (
-        <AudioPlayer
-          key={song.id}
-          song={song}
-          name={song.attributes.name}
-          url={song.attributes.audio.data[0].attributes.url}
-        />
+        <AudioPlayer key={song.id} song={song} />
       ))}
     </div>
   )
